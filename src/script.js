d301766157c937eda8dc4cc3cefcae3acdc2f507
tom <lim1017@hotmail.com>
2024-02-14 22:55:14 -0500

    moving starfield in background

--- src/script.js
+++ src/script.js
@@ -1,69 +1,104 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "dat.gui";
+
+const loader = new THREE.TextureLoader();
+const star = loader.load("./star.jpg");
 
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI();
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const geometry = new THREE.TorusGeometry(0.7, 0.2, 16, 100);
+
+const particleGeometry = new THREE.BufferGeometry();
+const particleCount = 5000;
+
+const posArray = new Float32Array(particleCount * 3);
+posArray.forEach((ele, idx) => {
+  posArray[idx] = (Math.random() - 0.5) * 4;
+});
+
+particleGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(posArray, 3)
+);
 
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const material = new THREE.PointsMaterial({
+  color: 0xfff000,
+  transparent: true,
+  size: 0.01,
+});
+
+const backgroundMaterial = new THREE.PointsMaterial({
+  transparent: true,
+  size: 0.005,
+  map: star,
+});
 
 // Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+const sphere = new THREE.Points(geometry, material);
+scene.add(sphere);
+
+const particleBackground = new THREE.Points(
+  particleGeometry,
+  backgroundMaterial
+);
+scene.add(particleBackground);
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+const pointLight = new THREE.PointLight(0xffffff, 0.1);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 2;
+scene.add(camera);
 
 // Controls
 // const controls = new OrbitControls(camera, canvas)
@@ -73,33 +108,48 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+// renderer.setClearColor(new THREE.Color("#21282a"), 1); //set background color
+
+//Mouse
+document.addEventListener("mousemove", animateParticles);
+let mouseX = 0;
+let mouseY = 0;
+
+function animateParticles(event) {
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+}
 
 /**
  * Animate
  */
 
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
-const tick = () =>
-{
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
 
-    const elapsedTime = clock.getElapsedTime()
+  // Update objects Auto
+  sphere.rotation.y = 0.5 * elapsedTime;
+  particleBackground.rotation.y = -0.1 * elapsedTime;
 
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+  if (mouseX > 0) {
+    //mouse movement update
+    particleBackground.rotation.y = mouseY * elapsedTime * 0.00008;
+  }
 
-    // Update Orbital Controls
-    // controls.update()
+  // Update Orbital Controls
+  // controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera);
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
 
-tick()
+tick();
